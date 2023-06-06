97488e46093e7fd3291ab3216526a2e606281cd0
vipinpaul <vipin.paul@bridgeconn.com>
2023-06-06 13:16:25 +0530

    initializing git and creating repo in door43

--- renderer/src/components/Sync/Ag/SyncToGitea.js
+++ renderer/src/components/Sync/Ag/SyncToGitea.js
@@ -1,97 +1,51 @@
 import * as localForage from 'localforage';
+import { environment } from 'environment';
 import * as logger from '../../../logger';
 import {
  handleCreateRepo, createFiletoServer, updateFiletoServer, getOrPutLastSyncInAgSettings,
 } from './SyncToGiteaUtils';
 import packageInfo from '../../../../../package.json';
-
+import { checkInitialize, commitChanges, initProject } from '../Isomorphic/utils';
+import { createRepo } from '../Isomorphic/api';
 // upload project to gitea main function
 export async function uploadToGitea(projectDataAg, auth, setSyncProgress, notifyStatus, addNotification) {
-    logger.debug('ToGiteaUtils.js', 'in uploadTOGitea');
-    const projectData = projectDataAg.projectMeta;
-    const projectId = Object.keys(projectData.identification.primary[packageInfo.name])[0];
-    const projectName = projectData.identification.name.en;
-    const ingredientsObj = projectData.ingredients;
-    const projectCreated = projectData.meta.dateCreated.split('T')[0];
-    const repoName = `${packageInfo.name}-${projectData.languages[0].tag}-${projectData.type.flavorType.flavor.name}-${projectName.replace(/[\s+ -]/g, '_')}`;
-
-    localForage.getItem('userProfile').then(async (user) => {
-      const newpath = localStorage.getItem('userPath');
-      const fs = window.require('fs');
-      const path = require('path');
-      const projectsMetaPath = path.join(newpath, packageInfo.name, 'users', user?.username, 'projects', `${projectName}_${projectId}`);
-      setSyncProgress((prev) => (
-        {
-          ...prev,
-          totalFiles: Object.keys(ingredientsObj).length,
+  logger.debug('ToGiteaUtils.js', 'in uploadTOGitea');
+  const projectData = projectDataAg.projectMeta;
+  const projectId = Object.keys(projectData.identification.primary[packageInfo.name])[0];
+  const projectName = projectData.identification.name.en;
+  const ingredientsObj = projectData.ingredients;
+  const projectCreated = projectData.meta.dateCreated.split('T')[0];
+  const repoName = `${projectData.languages[0].tag}-${projectData.type.flavorType.flavor.name}-${projectName.replace(/[\s+ -]/g, '_')}`;
+  // const repo = { name: `${projectData.languages[0].tag}-${projectData.type.flavorType.flavor.name}-${projectName.replace(/[\s+ -]/g, '_')}`, owner: { username: dcsOwners.length > 0 ? dcsOwners[0].username : auth.user.username } };
+  const branch = `${auth?.user?.username}/${packageInfo.name}`;
+  localForage.getItem('userProfile').then(async (user) => {
+    const newpath = localStorage.getItem('userPath');
+    const fs = window.require('fs');
+    const path = require('path');
+    const projectsMetaPath = path.join(newpath, packageInfo.name, 'users', user?.username, 'projects', `${projectName}_${projectId}`);
+    setSyncProgress((prev) => (
+      {
+        ...prev,
+        totalFiles: Object.keys(ingredientsObj).length,
+      }
+    ));
+    // Create A REPO for the project
+    try {
+      // Check whether the project is git initiallized or not
+      console.log(projectsMetaPath, branch);
+      const checkInit = await checkInitialize(fs, projectsMetaPath);
+      console.log(checkInit);
+      if (!checkInit) {
+        const projectInitialized = initProject(fs, projectsMetaPath, branch);
+        console.log(projectInitialized);
+        if (projectInitialized) {
+          console.log(repoName, auth.token.sha1);
+          const created = createRepo(repoName, auth.token.sha1);
+          console.log(created);
         }
-      ));
-      // Create A REPO for the project
-      try {
-        const createResult = await handleCreateRepo(repoName.toLowerCase(), auth);
-        if (createResult.id) {
-          logger.debug('SyncToGitea.js', 'repo created success : starting sync');
-          setSyncProgress((prev) => ({
-            ...prev,
-            syncStarted: true,
-          }));
-          // read metadata
-          const Metadata = await fs.readFileSync(path.join(projectsMetaPath, 'metadata.json'));
-          await createFiletoServer(JSON.stringify(Metadata), 'metadata.json', `${user?.username}/${projectCreated}.1`, createResult.name, auth);
-          logger.debug('SyncToGitea.js', 'read and uploaded metaData to repo');
-          // Read ingredients
-          /* eslint-disable no-await-in-loop */
-          /* eslint-disable no-restricted-syntax */
-          for (const key in ingredientsObj) {
-            if (Object.prototype.hasOwnProperty.call(ingredientsObj, key)) {
-              setSyncProgress((prev) => ({
-                ...prev,
-                completedFiles: prev.completedFiles + 1,
-              }));
-              // setTotalUploadedAg((prev) => prev + 1);
-              const Metadata1 = fs.readFileSync(path.join(projectsMetaPath, key), 'utf8');
-              await createFiletoServer(Metadata1, key, `${user?.username}/${projectCreated}.1`, createResult.name, auth);
-              logger.debug('SyncToGitea.js', `Read and uploaded ${key} to repo`);
-            }
-          }
-          // update the scribe-settings - sync
-          await getOrPutLastSyncInAgSettings('put', projectData, auth?.user?.username);
-          notifyStatus('success', 'Sync completed successfully !!');
-          await addNotification('Sync', 'Project Sync Successfull', 'success');
-          logger.debug('SyncToGitea.js', 'sync successfull - first time');
-        } else if (createResult.message.includes('409')) {
-          logger.debug('SyncToGitea.js', 'repo exist update section : 409 error');
-          setSyncProgress((prev) => ({
-            ...prev,
-            syncStarted: true,
-          }));
-          const metadataContent = fs.readFileSync(path.join(projectsMetaPath, 'metadata.json'));
-          await updateFiletoServer(JSON.stringify(metadataContent), 'metadata.json', `${user?.username}/${projectCreated}.1`, repoName, auth);
-          logger.debug('SyncToGitea.js', 'read and updated metaData to repo');
-          // Read ingredients and update
-          for (const key in ingredientsObj) {
-            if (Object.prototype.hasOwnProperty.call(ingredientsObj, key)) {
-              setSyncProgress((prev) => ({
-                ...prev,
-                completedFiles: prev.completedFiles + 1,
-              }));
-              const metadata1 = await fs.readFileSync(path.join(projectsMetaPath, key), 'utf8');
-              // setTotalUploadedAg((prev) => prev + 1);
-              await updateFiletoServer(metadata1, key, `${user?.username}/${projectCreated}.1`, repoName, auth);
-              logger.debug('SyncToGitea.js', `Read and updated ${key} to repo`);
-            }
-          }
-          // update the scribe-settings - sync update
-          await getOrPutLastSyncInAgSettings('put', projectData, auth?.user?.username);
-          logger.debug('SyncToGitea.js', 'Updated last Sync data');
-          notifyStatus('success', 'Sync completed successfully !!');
-          await addNotification('Sync', 'Project Sync Successfull', 'success');
-          logger.debug('SyncToGitea.js', 'sync successfull - update sync');
-        } else {
-          logger.debug('SyncToGitea.js', 'create/update not success on sync : ', createResult.message);
-          notifyStatus('failure', 'Something went wrong, sync failed, check internet');
-          logger.debug('SyncToGitea.js', 'sync failed - may be due to internet');
-        }
+      // } else {
+      //   commitChanges(pro);
+      }
     } catch (err) {
       logger.debug('SyncToGitea.js', `Error on Sync create/update : ${err}`);
       notifyStatus('failure', `Sync failed : ${err}`);
---
+++ renderer/src/components/Sync/Isomorphic/api.js
@@ -0,0 +1,21 @@
+import { environment } from "environment";
+
+// api for creating a repo in git
+export const createRepo= async(repoName,token)=>{
+  const endpoint=`${environment.GITEA_API_ENDPOINT}/user/repos`
+  const myHeaders = new Headers();
+  myHeaders.append('Authorization', `Bearer ${token}`);
+  myHeaders.append('Content-Type', 'application/json');
+  const payloadPr = JSON.stringify({
+    name: repoName,
+  });
+  const requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: payloadPr,
+    redirect: 'follow',
+  };
+  const fetchResult = await fetch(endpoint, requestOptions);
+  const result = await fetchResult.json();
+  return result;
+}
