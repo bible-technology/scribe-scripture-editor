bcd52dbcea0c68f905189b971987d9a1a5825de9
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-06-07 12:47:26 +0530

    progress updation for cloud sync

--- renderer/src/components/Sync/Ag/SyncToGitea.js
+++ renderer/src/components/Sync/Ag/SyncToGitea.js
@@ -8,15 +8,15 @@ import packageInfo from '../../../../../package.json';
 import {
  addGitRemote, checkInitialize, commitChanges, initProject, pushTheChanges,
 } from '../Isomorphic/utils';
-import { createRepo } from '../Isomorphic/api';
+// import { createRepo } from '../Isomorphic/api';
 // upload project to gitea main function
 export async function uploadToGitea(projectDataAg, auth, setSyncProgress, notifyStatus, addNotification) {
   logger.debug('ToGiteaUtils.js', 'in uploadTOGitea');
   const projectData = projectDataAg.projectMeta;
   const projectId = Object.keys(projectData.identification.primary[packageInfo.name])[0];
   const projectName = projectData.identification.name.en;
-  const ingredientsObj = projectData.ingredients;
-  const projectCreated = projectData.meta.dateCreated.split('T')[0];
+  // const ingredientsObj = projectData.ingredients;
+  // const projectCreated = projectData.meta.dateCreated.split('T')[0];
   const repoName = `${projectData.languages[0].tag}-${projectData.type.flavorType.flavor.name}-${projectName.replace(/[\s+ -]/g, '_')}`;
   // const repo = { name: `${projectData.languages[0].tag}-${projectData.type.flavorType.flavor.name}-${projectName.replace(/[\s+ -]/g, '_')}`, owner: { username: dcsOwners.length > 0 ? dcsOwners[0].username : auth.user.username } };
   const branch = `${auth?.user?.username}/${packageInfo.name}`;
@@ -25,12 +25,6 @@ export async function uploadToGitea(projectDataAg, auth, setSyncProgress, notify
     const fs = window.require('fs');
     const path = require('path');
     const projectsMetaPath = path.join(newpath, packageInfo.name, 'users', user?.username, 'projects', `${projectName}_${projectId}`);
-    setSyncProgress((prev) => (
-      {
-        ...prev,
-        totalFiles: Object.keys(ingredientsObj).length,
-      }
-    ));
     // Create A REPO for the project
     try {
       // Check whether the project is git initiallized or not
@@ -39,26 +33,38 @@ export async function uploadToGitea(projectDataAg, auth, setSyncProgress, notify
       const checkInit = await checkInitialize(fs, projectsMetaPath);
       console.log(checkInit);
       if (!checkInit) {
+        setSyncProgress((prev) => ({
+        ...prev, syncStarted: true, completedFiles: 1, totalFiles: 6,
+        }));
         remoteStatus = false;
         const projectInitialized = await initProject(fs, projectsMetaPath, auth.user.username, branch);
         console.log(projectInitialized);
         if (projectInitialized) {
+          setSyncProgress((prev) => ({ ...prev, completedFiles: prev.completedFiles + 1 }));
           console.log(repoName, auth.token.sha1);
           // const created = await createRepo(repoName, auth.token.sha1);
           const created = await handleCreateRepo(repoName, auth, repoName);
+          setSyncProgress((prev) => ({ ...prev, completedFiles: prev.completedFiles + 1 }));
           console.log(created);
           if (created.id) {
             remoteStatus = await addGitRemote(fs, projectsMetaPath, created.clone_url);
             console.log({ remoteStatus });
+            setSyncProgress((prev) => ({ ...prev, completedFiles: prev.completedFiles + 1 }));
           }
         }
+      } else {
+        setSyncProgress((prev) => ({
+        ...prev, syncStarted: true, completedFiles: 1, totalFiles: 3,
+        }));
       }
       if (remoteStatus) {
         const commitStatus = await commitChanges(fs, projectsMetaPath, { email: auth.user.email, username: auth.user.username }, 'Added from scribe');
         console.log({ commitStatus });
         if (commitStatus) {
+          setSyncProgress((prev) => ({ ...prev, completedFiles: prev.completedFiles + 1 }));
           const pushResult = await pushTheChanges(fs, projectsMetaPath, branch, auth.token.sha1);
           console.log({ pushResult });
+          setSyncProgress((prev) => ({ ...prev, completedFiles: prev.completedFiles + 1 }));
         }
       }
     } catch (err) {
