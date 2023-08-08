b592aff3678594655deff3f18a3392869d130084
bobby <bobbykumar706584@gmail.com>
2023-08-08 12:43:39 +0530

    playwright configuration and version updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,16 +2,20 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	testDir: './e2e-tests',
-	maxFailures: 2,
-	timeout: 60 * 1000,
-	globalTimeout: 60 * 60 * 1000,
+	maxFailures: process.env.CI ? 2 : 0,
+	timeout: 5 * 60 * 1000,
+	globalTimeout: process.env.CI ? 60 * 60 * 1000 : undefined,
+	// Run all tests in parallel.
+	fullyParallel: true,
 	retries: 3,
 	use: {
 		// trace: 'on-first-retry', // record traces on first retry of each test
-		headless: true,
+		 
+		// Run browser in headless mode.
+		headless: false,
 	  },
 	expect: {
-		timeout: 30 * 1000,
+		timeout: 60 * 1000,
 	},  
 };
 
