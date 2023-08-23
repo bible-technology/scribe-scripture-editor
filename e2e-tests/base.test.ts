c0533b9a14f4f98da3f2c231ec98436dd878f402
bobby <bobbykumar706584@gmail.com>
2023-08-23 08:50:54 +0530

    logged in/out, create new user

--- e2e-tests/base.test.ts
+++ e2e-tests/base.test.ts
@@ -3,12 +3,13 @@
 
 import { test, expect} from './myFixtures';
 import packageInfo from '../package.json';
+import ProjectList from '@/modules/projects/ProjectList';
 
 // import { _electron as electron } from 'playwright';
 import { removeUser } from '../renderer/src/core/Login/removeUser';
 
 const fs = require('fs');
-const { _electron: electron } = require('playwright');
+const { _electron: electron } = require('@playwright/test');
 
 let electronApp;
 let appPath;
@@ -19,6 +20,7 @@ test.describe('Scribe scripture editor', async() => {
   test.beforeAll(async() => {
       electronApp = await electron.launch({ args: ['main/index.js']} );
         window = await electronApp.firstWindow();
+        //application title
         expect(await window.title()).toBe('Scribe Scripture');
   });
 
@@ -34,27 +36,45 @@ test.afterAll(async() => {
 }); 
 
 test('Start with the new user created', async ({userName}) => {
-  const newpath = await window.evaluate(() =>  Object.assign({}, window.localStorage))
-  console.log(newpath)
-  const path = require('path');
-  const folder = path.join(newpath.userPath, packageInfo.name, 'users', userName.toLowerCase());
-  const file = path.join(newpath.userPath, packageInfo.name, 'users', 'users.json');
-  const data = await fs.readFileSync(file);
-  const json = JSON.parse(data);
-  user = json.filter((item) => item.username.toLowerCase() !== userName.toLowerCase())
-  const title = await window.textContent('[aria-label=projects]');
-    if(title === "Projects"){
-      await expect(title).toBe('Projects')
-      await window.getByRole('button', {name: "Open user menu"}).click()
-      await window.getByRole('menuitem', {name: "Sign out"}).click()
-      expect(await window.title()).toBe('Scribe Scripture');
-      if (await fs.existsSync(folder)) {
-        fs.rmdirSync(folder, {recursive: true})
-      }
-      console.error('users.json', 'removing data from json');
-      const filtered = json.filter((item) => item.username.toLowerCase() !== userName.toLowerCase())
-      return await fs.writeFileSync(file, JSON.stringify(filtered))
+  const textVisible = await window.locator('//h1["@aria-label=projects"]').isVisible()
+  test.setTimeout(2000)
+  if (textVisible) {
+    const newpath = await window.evaluate(() =>  Object.assign({}, window.localStorage))
+    const path = require('path');
+    const folder = path.join(newpath.userPath, packageInfo.name, 'users', userName.toLowerCase());
+    const file = path.join(newpath.userPath, packageInfo.name, 'users', 'users.json');
+    const data = await fs.readFileSync(file);
+    const json = JSON.parse(data);
+    const title = await window.textContent('[aria-label=projects]');
+    await expect(title).toBe('Projects')
+    await window.getByRole('button', {name: "Open user menu"}).click()
+    const currentUser = await window.textContent('[aria-label="userName"]')
+    await window.getByRole('menuitem', {name: "Sign out"}).click()
+    expect(await window.title()).toBe('Scribe Scripture');
+    if (await fs.existsSync(folder)) {
+      fs.rmdirSync(folder, {recursive: true})
     }
+    console.error('users.json', `${userName} data removed from json`);
+    const filtered = json.filter((item) => 
+      item.username.toLowerCase() !== currentUser.toLowerCase()
+    )
+    console.log(filtered)
+    return await fs.writeFileSync(file, JSON.stringify(filtered))
+  }else{
+    await window.getByRole('button', {name: 'Create New Account'}).click()
+    await expect(window.locator('//input[@placeholder="Username"]')).toBeVisible()
+    await expect(window.locator('//button[@type="submit"]')).toBeVisible()
+    await window.getByPlaceholder('Username').fill(userName)
+    await window.click('[type=submit]');
+    await window.waitForTimeout(2000)
+    const title = await window.textContent('[aria-label=projects]');
+    expect(title).toBe('Projects');
+  }
+    
   });
+
+  test('Delete playwright user', async ({userName}) => {
+
+  })
 
 })
