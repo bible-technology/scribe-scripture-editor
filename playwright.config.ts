fcaa224e7142e1954355f520599c0e673c954a0a
bobby <bobbykumar706584@gmail.com>
2023-08-24 17:39:45 +0530

    playwright login test

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,7 @@ const config: PlaywrightTestConfig = {
 	timeout: 5 * 60 * 1000,
 	globalTimeout: 60 * 60 * 1000,
 	// Run all tests in parallel.
-	fullyParallel: true,
+	fullyParallel: false,
 	retries: 3,	
 	use: {
 		// trace: 'on-first-retry', // record traces on first retry of each test
