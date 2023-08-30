6ce1cb4253efe5d5d4c1d8987450c616e4e25352
Arpit Jacob <arpit.jacob@gmail.com>
2023-08-30 10:38:32 +0200

    few more fixes

--- pages/wip/data/fileData.js
+++ pages/wip/data/fileData.js
@@ -19,4 +19,24 @@ export const fileData = [
     original: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
     new: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
   },
+  {
+    number: '5',
+    original: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+    new: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+  },
+  {
+    number: '6',
+    original: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+    new: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+  },
+  {
+    number: '7',
+    original: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+    new: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+  },
+  {
+    number: '7',
+    original: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+    new: 'This is how God made everything in the beginning. He created the universe and everything in it in six days. After God created the earth it was dark and empty because he had not yet formed anything in it. But God’s Spirit was there over the water.',
+  },
 ];
--- pages/wip/files.js
+++ pages/wip/files.js
@@ -6,7 +6,7 @@ import {
 import { fileData } from './data/fileData';
 
 const files = () => (
-  <div className="p-2 flex gap-2">
+  <div className="p-2 flex gap-2 h-screen">
     <div className="bg-white border-2 rounded-md border-black">
       <div className="flex items-center justify-between bg-black py-1.5 px-2.5">
         <span className="px-2.5 py-0.5 bg-primary text-white font-semibold tracking-wider text-xs uppercase rounded-xl">
@@ -15,8 +15,8 @@ const files = () => (
         <Cog8ToothIcon className="w-5 h-5 text-white" />
       </div>
       <ul className="text-black text-xs pt-2.5">
-        <li className="hover:bg-primary-100 px-5 py-2 cursor-pointer">bible/path/file_number_1.md</li>
-        <li className="hover:bg-primary-100 px-5 py-2 cursor-pointer">bible/path/file_number_2.md</li>
+        <li className="hover:bg-primary hover:text-white px-3 py-2 cursor-pointer rounded-md mx-1">bible/path/file_number_1.md</li>
+        <li className="hover:bg-primary hover:text-white px-3 py-2 cursor-pointer rounded-md mx-1">bible/path/file_number_2.md</li>
       </ul>
     </div>
     <div className="bg-white flex-1 border-2 border-gray-100 rounded-md">
@@ -36,7 +36,7 @@ const files = () => (
           <Cog8ToothIcon className="w-5 h-5 text-white" />
         </div>
       </div>
-      <div className="divide-y divide-gray-100">
+      <div className="divide-y h-full overflow-y-auto overflow-x-hidden pb-8 divide-gray-100">
         {fileData.map((file) => (
           <div key={file.number} className="flex px-2 py-6 gap-4">
             <div className="h-4 w-4 py-1 px-2 mt-1 flex items-center justify-center bg-primary rounded-full text-xxs text-white">
