ae4eec9987cdeee8a74de329c563e2f0d9dacb5f
Ntare cedrick <ntarecedrick250@gmail.com>
2023-08-31 15:22:25 +0200

    file view fix

--- pages/wip/files.js
+++ pages/wip/files.js
@@ -1,12 +1,14 @@
 import { Cog8ToothIcon } from '@heroicons/react/24/outline';
-import {
- ArrowSmallDownIcon, ArrowSmallUpIcon, ArrowsUpDownIcon,
-} from '@heroicons/react/20/solid';
+import { useState } from 'react';
+import ViewFileData from './components/ViewFileData';
 
 import { fileData } from './data/fileData';
 
-const files = () => (
-  <div className="p-2 flex gap-2 h-screen">
+const Files = () => {
+const [isComplete, setIsComplete] = useState(false);
+
+return (
+  <div className="p-2 flex gap-2">
     <div className="bg-white border-2 rounded-md border-black">
       <div className="flex items-center justify-between bg-black py-1.5 px-2.5">
         <span className="px-2.5 py-0.5 bg-primary text-white font-semibold tracking-wider text-xs uppercase rounded-xl">
@@ -15,8 +17,8 @@ const files = () => (
         <Cog8ToothIcon className="w-5 h-5 text-white" />
       </div>
       <ul className="text-black text-xs pt-2.5">
-        <li className="hover:bg-primary hover:text-white px-3 py-2 cursor-pointer rounded-md mx-1">bible/path/file_number_1.md</li>
-        <li className="hover:bg-primary hover:text-white px-3 py-2 cursor-pointer rounded-md mx-1">bible/path/file_number_2.md</li>
+        <li className="hover:bg-primary px-5 py-2 cursor-pointer line-through ">bible/path/file_number_1.md</li>
+        <li className="hover:bg-primary px-5 py-2 cursor-pointer">bible/path/file_number_2.md</li>
       </ul>
     </div>
     <div className="bg-white flex-1 border-2 border-gray-100 rounded-md">
@@ -36,39 +38,33 @@ const files = () => (
           <Cog8ToothIcon className="w-5 h-5 text-white" />
         </div>
       </div>
-      <div className="divide-y h-full overflow-y-auto overflow-x-hidden pb-8 divide-gray-100">
-        {fileData.map((file) => (
-          <div key={file.number} className="flex px-2 py-6 gap-4">
-            <div className="h-4 w-4 py-1 px-2 mt-1 flex items-center justify-center bg-primary rounded-full text-xxs text-white">
-              {file.number}
-            </div>
-
-            <div className="flex flex-col text-sm gap-2">
-              <div className="flex gap-2">
-                <span className="tracking-wide">{file.original}</span>
-              </div>
-              <div className="flex gap-2 items-end">
-                <span className="text-success tracking-wide">{file.new}</span>
-              </div>
-            </div>
-
-            <div className="flex flex-col justify-between self-stretch">
-              <div className="bg-black w-6 h-6 rounded-full flex justify-center items-center">
-                <ArrowSmallUpIcon className="w-4 h-4 text-white " />
-              </div>
-              <div className="bg-blue-500 w-6 h-6 rounded-full flex justify-center items-center">
-                <ArrowsUpDownIcon className="w-4 h-4 text-white" />
-              </div>
-              <div className="bg-success w-6 h-6 rounded-full flex justify-center items-center">
-                <ArrowSmallDownIcon className="w-5 h-5 text-white" />
-              </div>
-            </div>
-          </div>
+      <div className="divide-y divide-gray-100">
+        <div className="h-[42.5rem] overflow-auto">
+          {fileData.map((file) => (
+            <ViewFileData file={file} key={file.number} />
      ))}
+        </div>
+        <div className="flex justify-end">
+          {isComplete && (
+          <button type="button" className="px-4 py-1 bg-success uppercase text-sm rounded-md m-2 text-white">
+            Done
+          </button>
+)}
+          <button
+            type="button"
+            className="disabled:bg-gray-300 uppercase bg-success px-4 text-sm py-1 text-white disabled:text-black rounded-md m-2 disabled:cursor-not-allowed "
+            onClick={() => {
+              setIsComplete(true);
+          }}
+          >
+            Save
+          </button>
+        </div>
       </div>
     </div>
   </div>
 
   );
+};
 
-export default files;
+export default Files;
