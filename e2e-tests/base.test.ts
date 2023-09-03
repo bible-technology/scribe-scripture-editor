7230ee4d32e6af7e709e6100828ccaff6d073712
Bobbykumar706584 <bobbykumar706584@gmail.com>
2023-09-03 15:54:15 +0530

    if user logged in then delete

--- e2e-tests/base.test.ts
+++ e2e-tests/base.test.ts
@@ -10,7 +10,8 @@ let electronApp;
 let appPath;
 let window;
 
-test.beforeEach(async () => {
+
+test("Render the scribe   application", async () => {
   electronApp = await electron.launch({ args: ['main/index.js'] });
   appPath = await electronApp.evaluate(async ({ app }) => {
     // This runs in the main Electron process, parameter here is always
@@ -22,10 +23,24 @@ test.beforeEach(async () => {
   expect(await window.title()).toBe('Scribe Scripture');
   await window.waitForSelector('//*[@id="__next"]/div[1]', '//*[@id="__next"]/div')
 
+})
+
+test('Check whether the app is being logged IN', async ({ userName }) => {
+  const textVisble = await window.locator('//h1["@aria-label=projects"]').isVisible()
+  if (textVisble) {
+    const title = await window.textContent('[aria-label=projects]')
+    console.log('user is Logged In')
+    await expect(title).toBe('Projects')
+
+  } else {
+    const welcome = await window.textContent('//*[@id="__next"]/div/div[1]/div/h2')
+    console.log('user is not Logged In')
+    await expect(welcome).toBe("Welcome!")
+  }
 });
 
-test('Check whether the app is being logged IN', async ({userName}) => {
-  const newpath = await window.evaluate(() =>  Object.assign({}, window.localStorage))
+test('If logged IN then logout and delete that user from the backend', async ({ userName }) => {
+  const newpath = await window.evaluate(() => Object.assign({}, window.localStorage))
   const path = require('path');
   const folder = path.join(newpath.userPath, packageInfo.name, 'users', userName.toLowerCase());
   const file = path.join(newpath.userPath, packageInfo.name, 'users', 'users.json');
@@ -35,12 +50,44 @@ test('Check whether the app is being logged IN', async ({userName}) => {
 
   if (textVisble) {
     const title = await window.textContent('[aria-label=projects]')
-    console.log('user is Logged In')
+
     await expect(title).toBe('Projects')
-    
-  }else {
-    const welcome = await window.textContent('//*[@id="__next"]/div/div[1]/div/h2')
-    console.log('user is not Logged In')
-    await expect(welcome).toBe("Welcome!")
+    await window.getByRole('button', { name: "Open user menu" }).click()
+    const currentUser = await window.textContent('[aria-label="userName"]')
+    if (currentUser !== userName.toLowerCase()) {
+      await window.getByRole('menuitem', { name: "Sign out" }).click()
+      console.log(`current user is not a ${userName}`)
+    } else {
+      await window.getByRole('menuitem', { name: "Sign out" }).click()
+      if (currentUser === userName.toLowerCase() && await fs.existsSync(folder)) {
+        fs.rmSync(folder, { recursive: true, force: true })
+
+      }
+      console.error('users.json', `${userName} data removed from json`);
+      const filtered = json.filter((item) =>
+        item.username.toLowerCase() !== userName.toLowerCase()
+      )
+      console.log(`user is ${userName}`)
+      await fs.writeFileSync(file, JSON.stringify(filtered))
+      expect(await window.title()).toBe('Scribe Scripture');
+    }
+
+  } else {
+    if (await window.locator('//*[@id="playwright user"]').isVisible()) {
+      if (userName.toLowerCase() && await fs.existsSync(folder)) {
+        fs.rmSync(folder, { recursive: true, force: true })
+
+      }
+      console.error('users.json', `${userName} data removed from json`);
+      const filtered = json.filter((item) =>
+        item.username.toLowerCase() !== userName.toLowerCase()
+      )
+      console.log(`user is ${userName}`)
+      await fs.writeFileSync(file, JSON.stringify(filtered))
+      expect(await window.title()).toBe('Scribe Scripture');
+    } else {
+      const welcome = await window.textContent('//*[@id="__next"]/div/div[1]/div/h2')
+      await expect(welcome).toBe("Welcome!")
+    }
   }
 });
