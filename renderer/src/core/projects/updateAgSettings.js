f2022dbfa66fe90ad47baab3f977a1de0f7e7c92
vipinpaul <vipin.paul@bridgeconn.com>
2023-11-21 16:10:58 +0530

    debugging

--- renderer/src/core/projects/updateAgSettings.js
+++ renderer/src/core/projects/updateAgSettings.js
@@ -13,6 +13,7 @@ import {
 // }
 
 export const updateAgSettings = async (username, projectName, data, font) => {
+  console.log(JSON.stringify(data.project[data.type.flavorType.flavor.name]));
   logger.debug('updateAgSettings.js', 'In updateAgSettings');
   const newpath = localStorage.getItem('userPath');
   const fs = window.require('fs');
@@ -21,6 +22,7 @@ export const updateAgSettings = async (username, projectName, data, font) => {
   const folder = path.join(newpath, packageInfo.name, 'users', username, 'projects', projectName, result[0]);
   const settings = await fs.readFileSync(folder, 'utf8');
   const setting = JSON.parse(settings);
+  setting.project[data.type.flavorType.flavor.name] = data.project[data.type.flavorType.flavor.name];
   if (settings.version !== environment.AG_SETTING_VERSION) {
     setting.version = environment.AG_SETTING_VERSION;
     if (!setting.sync && !setting.sync?.services) {
@@ -34,7 +36,7 @@ export const updateAgSettings = async (username, projectName, data, font) => {
       setting.project[data.type.flavorType.flavor.name].font = font || setting.project[data.type.flavorType.flavor.name].font;
     }
   }
-  setting.project[data.type.flavorType.flavor.name] = data.project[data.type.flavorType.flavor.name];
+  console.log(JSON.stringify(setting));
   logger.debug('updateAgSettings.js', `Updating the ${environment.PROJECT_SETTING_FILE}`);
   await fs.writeFileSync(folder, JSON.stringify(setting));
 };
@@ -45,7 +47,6 @@ export const updateWebAgSettings = async (username, projectName, data) => {
   const { data: settings } = await sbStorageDownload(folder);
   let setting = {};
   setting = JSON.parse(await settings.text());
-  setting.project[data.type.flavorType.flavor.name] = data.project[data.type.flavorType.flavor.name];
   if (settings.version !== environment.AG_SETTING_VERSION) {
     setting.version = environment.AG_SETTING_VERSION;
     if (!setting.sync && !setting.sync?.services) {
