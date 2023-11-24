ca8bd36d1898e03273ad5aa237a5ff870a397eaf
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-11-24 17:17:22 +0530

    fixes for notes no file issue , removed question and response tags from ui

--- renderer/src/components/EditorPage/Reference/TranslationHelpsCard.js
+++ renderer/src/components/EditorPage/Reference/TranslationHelpsCard.js
@@ -67,43 +67,45 @@ export default function TranslationHelpsCard({
                   if (item?.identifier.toLowerCase() === projectId.toLowerCase()) {
                     return item;
                   }
-              });
-              const filecontent = await fs.readFileSync(path.join(folder, projectName, currentFile[0].path), 'utf8');
-              // convert tsv to json
-              const headerArr = filecontent.split('\n')[0].split('\t');
-              let noteName;
-              let indexOfNote;
-              if (headerArr.indexOf('Note') > 0) {
-                indexOfNote = headerArr.indexOf('Note');
-                noteName = headerArr[indexOfNote];
-              } else if (headerArr.indexOf('OccurrenceNote')) {
-                indexOfNote = headerArr.indexOf('OccurrenceNote');
-                noteName = headerArr[indexOfNote];
-              }
-
-              let bvcType = true;
-              if (headerArr.includes('Reference') && headerArr.every((value) => !['Book', 'Verse', 'Chapter'].includes(value))) {
-                bvcType = false;
-              }
-
-              const json = filecontent.split('\n')
-                .map((file) => {
-                  if (bvcType) {
-                    const [Book, Chapter, Verse, ID, SupportReference, OrigQuote, Occurrence, GLQuote, OccurrenceNote] = file.split('\t');
-                    return {
-                      Book, Chapter, Verse, ID, SupportReference, OrigQuote, Occurrence, GLQuote, OccurrenceNote,
-                      };
+                });
+                if (currentFile?.length > 0) {
+                  const filecontent = await fs.readFileSync(path.join(folder, projectName, currentFile[0].path), 'utf8');
+                  // convert tsv to json
+                  const headerArr = filecontent.split('\n')[0].split('\t');
+                  let noteName;
+                  let indexOfNote;
+                  if (headerArr.indexOf('Note') > 0) {
+                    indexOfNote = headerArr.indexOf('Note');
+                    noteName = headerArr[indexOfNote];
+                  } else if (headerArr.indexOf('OccurrenceNote')) {
+                    indexOfNote = headerArr.indexOf('OccurrenceNote');
+                    noteName = headerArr[indexOfNote];
                   }
-                    const Book = projectId;
-                    const [ref, ID] = file.split('\t');
-                    const Chapter = ref.split(':')[0];
-                    const Verse = ref.split(':')[1];
-                    return {
-                      Book, Chapter, Verse, ID, [noteName]: file.split('\t')[indexOfNote],
+
+                  let bvcType = true;
+                  if (headerArr.includes('Reference') && headerArr.every((value) => !['Book', 'Verse', 'Chapter'].includes(value))) {
+                    bvcType = false;
+                  }
+
+                  const json = filecontent.split('\n')
+                    .map((file) => {
+                      if (bvcType) {
+                        const [Book, Chapter, Verse, ID, SupportReference, OrigQuote, Occurrence, GLQuote, OccurrenceNote] = file.split('\t');
+                        return {
+                          Book, Chapter, Verse, ID, SupportReference, OrigQuote, Occurrence, GLQuote, OccurrenceNote,
+                        };
+                      }
+                      const Book = projectId;
+                      const [ref, ID] = file.split('\t');
+                      const Chapter = ref.split(':')[0];
+                      const Verse = ref.split(':')[1];
+                      return {
+                        Book, Chapter, Verse, ID, [noteName]: file.split('\t')[indexOfNote],
                       };
-                }).filter((data) => data.Chapter === chapter && data.Verse === verse);
-                setOfflineItemsDisable(false);
-                setOfflineItems(json);
+                    }).filter((data) => data.Chapter === chapter && data.Verse === verse);
+                  setOfflineItemsDisable(false);
+                  setOfflineItems(json);
+                }
               }
               break;
 
@@ -206,7 +208,7 @@ export default function TranslationHelpsCard({
   return (
     <ReferenceCard
       items={items}
-      filters={['OccurrenceNote', 'Question', 'Response']}
+      filters={['OccurrenceNote']}
       markdown={markdown}
       isLoading={isLoading}
       languageId={languageId}
