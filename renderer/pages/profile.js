5224deb98e74eeb81eda078a03822f00d7cd14ec
vipinpaul <vipin.paul@bridgeconn.com>
2023-11-27 12:34:30 +0530

    Fixed window title issue

--- renderer/pages/profile.js
+++ renderer/pages/profile.js
@@ -1,13 +1,17 @@
 import Profile from '@/modules/projects/Profile';
 import AuthenticationContextProvider from '@/components/Login/AuthenticationContextProvider';
 import ProjectContextProvider from '@/components/context/ProjectContext';
+import Meta from '../src/Meta';
 
 const profile = () => (
-  <AuthenticationContextProvider>
-    <ProjectContextProvider>
-      <Profile />
-    </ProjectContextProvider>
-  </AuthenticationContextProvider>
+  <>
+    <Meta />
+    <AuthenticationContextProvider>
+      <ProjectContextProvider>
+        <Profile />
+      </ProjectContextProvider>
+    </AuthenticationContextProvider>
+  </>
 );
 
 export default profile;
--- renderer/pages/projects.js
+++ renderer/pages/projects.js
@@ -2,15 +2,19 @@ import ProjectList from '@/components/Projects/ProjectList';
 import AuthenticationContextProvider from '@/components/Login/AuthenticationContextProvider';
 import AutographaContextProvider from '@/components/context/AutographaContext';
 import ProjectContextProvider from '@/components/context/ProjectContext';
+import Meta from '../src/Meta';
 
 const Projects = () => (
-  <AuthenticationContextProvider>
-    <AutographaContextProvider>
-      <ProjectContextProvider>
-        <ProjectList />
-      </ProjectContextProvider>
-    </AutographaContextProvider>
-  </AuthenticationContextProvider>
+  <>
+    <Meta />
+    <AuthenticationContextProvider>
+      <AutographaContextProvider>
+        <ProjectContextProvider>
+          <ProjectList />
+        </ProjectContextProvider>
+      </AutographaContextProvider>
+    </AuthenticationContextProvider>
+  </>
   );
 
 export default Projects;
