4819826c6bb6be04a6e465674877daf6779cf02f
vipinpaul <vipin.paul@bridgeconn.com>
2023-11-27 11:20:36 +0530

    Fixed the wrapping issue with Layout

--- renderer/pages/profile.js
+++ renderer/pages/profile.js
@@ -1,9 +1,12 @@
 import Profile from '@/modules/projects/Profile';
 import AuthenticationContextProvider from '@/components/Login/AuthenticationContextProvider';
+import ProjectContextProvider from '@/components/context/ProjectContext';
 
 const profile = () => (
   <AuthenticationContextProvider>
-    <Profile />
+    <ProjectContextProvider>
+      <Profile />
+    </ProjectContextProvider>
   </AuthenticationContextProvider>
 );
 
