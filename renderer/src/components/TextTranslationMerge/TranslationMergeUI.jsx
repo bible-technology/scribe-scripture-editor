cce5c0a9767ea0af3019c3c9b8a1916a83d6370c
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-15 14:44:05 +0530

    import file function added

--- renderer/src/components/TextTranslationMerge/TranslationMergeUI.jsx
+++ renderer/src/components/TextTranslationMerge/TranslationMergeUI.jsx
@@ -6,8 +6,13 @@ import { AutographaContext } from '@/components/context/AutographaContext';
 import { useTranslation } from 'react-i18next';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import ConfirmationModal from '@/layouts/editor/ConfirmationModal';
+import TranslationMergNavBar from './TranslationMergNavBar';
+import * as logger from '../../logger';
+import useGetCurrentProjectMeta from '../Sync/hooks/useGetCurrentProjectMeta';
 
 function TranslationMergeUI() {
+  const [importedUsfmFolderPath, setImportedUsfmFolderPath] = useState([]);
+
   const {
     states: {
       openTextTranslationMerge,
@@ -25,6 +30,13 @@ function TranslationMergeUI() {
     confirmMessage: '',
     buttonName: '',
   });
+
+  const {
+    state: { currentProjectMeta },
+    actions: { getProjectMeta },
+  } = useGetCurrentProjectMeta();
+
+  // console.log({ openTextTranslationMerge, currentProjectMeta, importedUsfmFolderPath });
 
   const modalClose = () => {
     setModel({
@@ -50,16 +62,43 @@ function TranslationMergeUI() {
   };
 
   const handleOnAbortMerge = () => {
-    console.log('Agreed on abort');
     modalClose();
-    setOpenTextTranslationMerge(false);
+    setOpenTextTranslationMerge({ open: false, meta: null });
+  };
+
+  useEffect(() => {
+    if (openTextTranslationMerge?.meta && !currentProjectMeta) {
+      const { id, name } = openTextTranslationMerge.meta;
+      (async () => {
+        await getProjectMeta(`${name}_${id[0]}`);
+      })();
+    }
+  }, []);
+
+  const openFileDialogSettingData = async () => {
+    logger.debug('translationMergeUI.js', 'Inside openFileDialogSettingData');
+    const options = {
+      properties: ['openFile'],
+      filters: [{ name: 'usfm files', extensions: ['usfm', 'sfm', 'USFM', 'SFM'] }],
+    };
+    const { dialog } = window.require('@electron/remote');
+    const chosenFolder = await dialog.showOpenDialog(options);
+    if ((chosenFolder.filePaths).length > 0) {
+      logger.debug('translationMergeUI.js', 'Selected the files');
+      setImportedUsfmFolderPath(chosenFolder.filePaths);
+    } else {
+      logger.debug('translationMergeUI.js', 'Didn\'t select any file');
+    }
+  };
+
+  const handleImportUsfm = () => {
+    openFileDialogSettingData();
   };
 
   return (
     <>
-
       <Transition
-        show={openTextTranslationMerge}
+        show={openTextTranslationMerge.open}
         as={Fragment}
         enter="transition duration-100 ease-out"
         enterFrom="transform scale-95 opacity-0"
@@ -73,13 +112,13 @@ function TranslationMergeUI() {
           className="fixed inset-0 z-10 overflow-y-auto"
           initialFocus={cancelButtonRef}
           static
-          open={openTextTranslationMerge}
+          open={openTextTranslationMerge.open}
           onClose={() => removeSection(true)}
         >
 
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
           <div className="flex flex-col mx-12 mt-10 fixed inset-0 z-10 overflow-y-auto items-center justify-center ">
-            <div className="bg-black relative flex justify-between px-3 items-center rounded-t-lg h-10 w-[80%]">
+            <div className="bg-black relative flex justify-between px-3 items-center rounded-t-lg h-10 w-[80vw]">
               <h1 className="text-white font-bold text-sm uppercase">{t('label-resolve-conflict')}</h1>
               <div aria-label="resources-search" className="pt-1.5 pb-[6.5px]  bg-secondary text-white text-xs tracking-widest leading-snug text-center" />
               {/* close btn section */}
@@ -93,7 +132,30 @@ function TranslationMergeUI() {
             </div>
 
             {/* contents section */}
-            content
+            <div className="bg-[#e7e7e7] w-[80vw] h-[80vh]">
+              <div className="p-2 h-full border grid grid-cols-6 gap-2">
+                <TranslationMergNavBar />
+                <div className="col-span-5 h-full flex flex-col gap-2">
+                  <div className="border-2 border-black rounded-md grow p-2">
+                    <div>
+                      Project Scope :
+                      {Object.keys(currentProjectMeta?.type?.flavorType?.currentScope).map((scope) => (
+                        <span className="ml-2" key={scope}>{scope}</span>
+                      ))}
+
+                    </div>
+                    <button
+                      type="button"
+                      className="border-2 border-primary rounded-lg px-2 py-1 hover:bg-primary hover:text-white"
+                      onClick={handleImportUsfm}
+                    >
+                      {`${t('btn-import')} Usfm`}
+                    </button>
+                  </div>
+                  <div className="border-2 border-black rounded-md h-[50px] p-1">Footer</div>
+                </div>
+              </div>
+            </div>
           </div>
         </Dialog>
       </Transition>
