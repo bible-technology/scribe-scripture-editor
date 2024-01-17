f629c45ee566c863d9f4a9abf5a732d9906bd9d1
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-17 11:23:25 +0530

    sample render of usfm diff for test

--- renderer/src/components/TextTranslationMerge/UsfmConflictEditor.jsx
+++ renderer/src/components/TextTranslationMerge/UsfmConflictEditor.jsx
@@ -1,8 +1,26 @@
+/* eslint-disable no-nested-ternary */
 import React from 'react';
 
 function UsfmConflictEditor({ usfmJsons }) {
+  console.log({ usfmJsons });
   return (
-    <div>UsfmConflictEditor</div>
+    <div className="">
+      <h5 className="text-center">Sample USFM render</h5>
+      <div className="mt-3 text-sm">
+        {usfmJsons.diffOut?.map((item, index) => (
+          <span
+            key={index}
+            className="px-1"
+            style={{
+              color: `${item[0] === 1 ? 'green' : item[0] === -1 ? 'red' : ''
+                }`,
+            }}
+          >
+            {item[1]}
+          </span>
+        ))}
+      </div>
+    </div>
   );
 }
 
