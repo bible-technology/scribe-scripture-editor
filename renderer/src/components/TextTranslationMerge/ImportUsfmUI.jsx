3b531fc6b095e142d64f8e5777e6e571d7f16158
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-02-15 11:44:47 +0530

    added functionality for finiahs conflict resolution

--- renderer/src/components/TextTranslationMerge/ImportUsfmUI.jsx
+++ renderer/src/components/TextTranslationMerge/ImportUsfmUI.jsx
@@ -31,23 +31,26 @@ function ImportUsfmUI({
         <div className="w-[.5px] h-full bg-gray-400" />
 
         {/* saved conflicts - resume */}
-        <div className="w-full flex-1 h-full flex flex-col justify-center items-center gap-2">
+        {savedConflictsBooks?.length > 0 && (
 
-          <h4 className="font-medium text-center">Resume Conflict Resolution</h4>
-          <div className="grid grid-cols-5 gap-2 overflow-y-auto">
+          <div className="w-full flex-1 h-full flex flex-col justify-center items-center gap-2">
 
-            {savedConflictsBooks.map((book) => (
-              <button
-                key={book}
-                type="button"
-                className="border-2 border-primary rounded-lg px-2 py-1 hover:bg-primary hover:text-white"
-                onClick={() => resumeConflictResolution(book)}
-              >
-                {book.toUpperCase().split('.')[0]}
-              </button>
-            ))}
+            <h4 className="font-medium text-center">Resume Conflict Resolution</h4>
+            <div className="grid grid-cols-5 gap-2 overflow-y-auto">
+
+              {savedConflictsBooks.map((book) => (
+                <button
+                  key={book}
+                  type="button"
+                  className="border-2 border-primary rounded-lg px-2 py-1 hover:bg-primary hover:text-white"
+                  onClick={() => resumeConflictResolution(book)}
+                >
+                  {book.toUpperCase().split('.')[0]}
+                </button>
+              ))}
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
