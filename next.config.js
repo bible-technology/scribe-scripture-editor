beab6abc493c7aed6a9a225ee4ef578df6a8d815
danielc-n <nicolas.condomitti@xenizo.fr>
2024-06-21 14:48:29 +0200

    updated nextjs config

--- next.config.js
+++ next.config.js
@@ -39,6 +39,7 @@ const nextConfig = {
 
     return config;
   },
+  output: "export",
   images: {
     // limit of 25 deviceSizes values
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
