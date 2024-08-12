7a15ec6751e1f5b20b025c97027b9421af43d36d
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-08-12 13:02:04 +0530

    refactor component of toggle select books

---
+++ renderer/src/components/ProjectManagement/scope-management/BulkSelectionGroup.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ToggleOptions = [
+  { key: 'all', name: 'All' },
+  { key: 'old', name: 'Old' },
+  { key: 'new', name: 'New' },
+  { key: 'custom', name: 'Custom' },
+];
+
+function BulkSelectionGroup({
+  selectedOption = ToggleOptions[3].key,
+  handleSelect,
+}) {
+  return (
+    <div className="px-2 h-8 flex gap-3 text-xs text-gray-800 items-center">
+      {ToggleOptions.map((option) => (
+        <div key={option.key} className="flex gap-1 items-center">
+          <input
+            type="radio"
+            value={option.key}
+            className="pr-2"
+            checked={selectedOption === option.key}
+            onChange={handleSelect}
+          />
+          <label className="">
+            {option.name}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BulkSelectionGroup;
--- renderer/src/components/ProjectManagement/scope-management/ScopeManagement.jsx
+++ renderer/src/components/ProjectManagement/scope-management/ScopeManagement.jsx
@@ -3,6 +3,7 @@ import { useBibleReference } from 'bible-reference-rcl';
 import ScopeHead from './ScopeHead';
 import TitleBar from './TitleBar';
 import BookButton from '../Common/Button/BookButton';
+import BulkSelectionGroup from './BulkSelectionGroup';
 
 const initialBook = 'gen';
 const initialChapter = '1';
@@ -49,45 +50,10 @@ function ScopeManagement() {
       <ScopeHead>Project Scope Management</ScopeHead>
       <TitleBar>
         <p className="text-gray-900 text-center text-sm">Book Selection</p>
-
-        <div className="px-2 h-8 flex gap-3 text-sm text-gray-800 items-center">
-          <div className="flex gap-1 items-center">
-            <input
-              type="radio"
-              value="all"
-              className="pr-2"
-              checked={selectedOption === 'all'}
-              onChange={handleChangeBookToggle}
-            />
-            <label className="">
-              All
-            </label>
-          </div>
-
-          <div className="flex gap-1 items-center">
-            <input
-              type="radio"
-              value="old"
-              checked={selectedOption === 'old'}
-              onChange={handleChangeBookToggle}
-            />
-            <label className="">
-              Old
-            </label>
-          </div>
-
-          <div className="flex gap-1 items-center">
-            <input
-              type="radio"
-              value="new"
-              checked={selectedOption === 'new'}
-              onChange={handleChangeBookToggle}
-            />
-            <label className="">
-              New
-            </label>
-          </div>
-        </div>
+        <BulkSelectionGroup
+          selectedOption={selectedOption}
+          handleSelect={handleChangeBookToggle}
+        />
       </TitleBar>
 
       <div className="grid grid-cols-2 gap-5">
