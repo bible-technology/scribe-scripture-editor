2a5ad72680ff3799893f46c177e62cfaa27d9354
vipinpaul <vipin.paul@bridgeconn.com>
2025-05-27 18:08:29 +0530

    Downloaded the equivalent tw for the selected twl and calling the tw internally to display the content using the twLinks

--- renderer/src/components/Resources/useHandleChangeQuery.js
+++ renderer/src/components/Resources/useHandleChangeQuery.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-nested-ternary */
 const handleChangeQuery = (query, resourceData, selectResource, setFilteredResources, subMenuItems, setfilteredBibleObsAudio) => {
-  const filtered = { offlineResource: [], onlineResource: { ...resourceData?.reference } || {} };
+  const filtered = { offlineResource: [], onlineResource: { ...resourceData?.reference } || {}, dependentResource: { ...resourceData?.dependentResource } };
   if (['tn', 'tir', 'tw', 'tq', 'ta', 'obs-tn', 'obs-tq', 'twl', 'obs-twlm'].includes(selectResource?.toLowerCase())) {
     if (query?.length > 0) {
       filtered.offlineResource = resourceData?.offlineResource?.filter((data) => {
