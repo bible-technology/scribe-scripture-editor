10c2f06cac4dce65256c9b619e9f1d0c098abd1c
Bernise.john <bernise.john@bridgeconn.com>
2025-08-26 10:21:37 +0530

    commented issues solved

--- renderer/src/components/Projects/ProjectRow.js
+++ renderer/src/components/Projects/ProjectRow.js
@@ -167,7 +167,7 @@ const ProjectRowItem = ({
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items
-                      className="min-w-[280px] max-w-[320px]  bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                      className="min-w-[240px] max-w-[260px]  bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                       style={menuStyle}
                     >
                       <div className="px-1 py-1">
