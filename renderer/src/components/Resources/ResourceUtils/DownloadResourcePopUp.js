a8f9af671a94f16050080caf0c8882f9884987e6
Bernise Jacob John <bernise.john@bridgeconn.com>
2025-09-23 17:10:46 +0530

    Update DownloadResourcePopUp.js

--- renderer/src/components/Resources/ResourceUtils/DownloadResourcePopUp.js
+++ renderer/src/components/Resources/ResourceUtils/DownloadResourcePopUp.js
@@ -56,29 +56,21 @@ function DownloadResourcePopUp({ selectResource, isOpenDonwloadPopUp, setIsOpenD
     // states: { resourceDownload },
     action: {
       setNotifications,
-      setActiveNotificationCount,
       // setResourceDownload,
     },
   } = React.useContext(AutographaContext);
 
   const addNewNotification = async (title, text, type) => {
     localForage.getItem('notification').then((value) => {
-      const temp = [...(value || [])];
-      const newNotification = {
+      const temp = [...value];
+      temp.push({
         title,
         text,
         type,
         time: moment().format(),
         hidden: true,
-        isRead: false,
-      };
-
-      temp.push(newNotification);
-      localForage.setItem('notification', temp).then(() => {
-        setNotifications(temp);
-        const unreadCount = temp.filter((n) => !n.isRead).length;
-        setActiveNotificationCount(unreadCount);
       });
+      setNotifications(temp);
     });
   };
 
