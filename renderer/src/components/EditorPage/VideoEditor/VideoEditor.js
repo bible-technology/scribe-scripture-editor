7a222854b09f7a2fb00dcf64932efe1f50b64189
Bernise.john <bernise.john@bridgeconn.com>
2026-01-07 18:13:17 +0530

    Resolved conflict

--- renderer/src/components/EditorPage/VideoEditor/VideoEditor.js
+++ renderer/src/components/EditorPage/VideoEditor/VideoEditor.js
@@ -785,26 +785,28 @@ const VideoEditor = ({ editor }) => {
       {isLoading && !displyScreen && <LoadingScreen />}
       {videoContent && isLoading === false
         && (
-          <VideoPlayer
-            verse={verse}
-            location={videoPath}
-            content={videoContent}
-            fontSize={editorFontSize}
-            selectedFont={selectedFont}
-            updateWave={updateVideoWave}
-            onChangeVerse={onChangeVerse}
-            onDeleteVideo={executeDeleteVideo}
-            onDisjoinVerse={onDisjoinVerse}
-            onJoinVerse={onJoinVerse}
-            setOpenModal={setModel}
-            chapter={chapter}
-            bookId={bookId}
-            pendingRecorderReopen={pendingRecorderReopen}
-            setPendingRecorderReopen={setPendingRecorderReopen}
-            setNotify={setNotify}
-            setSnackText={setSnackText}
-            setOpenSnackBar={setOpenSnackBar}
-          />
+          <div id="video-editor" className="h-full overflow-auto">
+            <VideoPlayer
+              verse={verse}
+              location={videoPath}
+              content={videoContent}
+              fontSize={editorFontSize}
+              selectedFont={selectedFont}
+              updateWave={updateVideoWave}
+              onChangeVerse={onChangeVerse}
+              onDeleteVideo={executeDeleteVideo}
+              onDisjoinVerse={onDisjoinVerse}
+              onJoinVerse={onJoinVerse}
+              setOpenModal={setModel}
+              chapter={chapter}
+              bookId={bookId}
+              pendingRecorderReopen={pendingRecorderReopen}
+              setPendingRecorderReopen={setPendingRecorderReopen}
+              setNotify={setNotify}
+              setSnackText={setSnackText}
+              setOpenSnackBar={setOpenSnackBar}
+            />
+          </div>
         )}
       <ConfirmationModal
         openModal={model.openModel}
